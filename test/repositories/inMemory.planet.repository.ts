3c3cafadfaae2765842bf4017eaa68f894098d7c
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-09-26 19:56:56 -0300

    feat: implementacao do metodo Either e criacao da classe InMemory para teste

---
+++ test/repositories/inMemory.planet.repository.ts
@@ -0,0 +1,14 @@
+import { Planet } from "src/domain/planet/entity/planet.entity";
+import { PlanetRepository } from "src/domain/planet/repository/planet.repository";
+
+export class InMemoryPlanetRepository implements PlanetRepository {
+
+    itens: Planet[] = [];
+
+    async create(planet: Planet): Promise<Planet> {
+        await this.itens.push(planet);
+
+        return planet;
+    }
+
+}
