b0fed7822a771944b6ef6fcad4b9887883960924
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-10-03 21:29:50 -0300

    feat: create entity spaceShip

--- src/domain/spaceShips/entity/spaceShips.entity.ts
+++ src/domain/spaceShips/entity/spaceShips.entity.ts
@@ -1,4 +1,5 @@
 import Entity from "src/core/generics/entity";
+import Idendity from "src/core/generics/identity";
 
 type SpaceShipType = {
     name: string;
@@ -7,4 +8,41 @@ type SpaceShipType = {
     passengerCapacity: number;
 }
 
-export class spaceShip extends Entity<SpaceShipType> { }
+export class SpaceShip extends Entity<SpaceShipType> {
+
+    static create(entity: SpaceShipType, id?: Idendity): SpaceShip {
+        return new SpaceShip({ ...entity }, id);
+    }
+
+    get name(): string {
+        return this.attributes.name;
+    }
+
+    get model(): string {
+        return this.attributes.model;
+    }
+
+    get manufacturer(): string {
+        return this.attributes.manufacturer;
+    }
+
+    get passengerCapacity(): number {
+        return this.attributes.passengerCapacity;
+    }
+
+    set name(name: string) {
+        this.attributes.name = name;
+    }
+
+    set model(model: string) {
+        this.attributes.model = model;
+    }
+
+    set manufacturer(manufacturer: string) {
+        this.attributes.manufacturer = manufacturer;
+    }
+
+    set passengerCapacity(passengerCapacity: number) {
+        this.attributes.passengerCapacity = passengerCapacity;
+    }
+}
