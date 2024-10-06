938d26e5958fde81ec5270aa229fee69bbdf1823
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-10-06 12:53:28 -0300

    feat: atutenticacao de rotas

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,13 +1,22 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./jwt.strategy";
+import { PassportModule } from "@nestjs/passport";
+import { APP_GUARD } from "@nestjs/core";
+import { JwtAuthGuard } from "./jwt.auth.guard";
 
 @Module({
     imports: [
+        PassportModule,
         JwtModule.register({
             global: true,
             secret: process.env.JWT_SECRET,
             signOptions: { expiresIn: '10m' }
         })
+    ],
+    providers: [
+        JwtStrategy,
+        { provide: APP_GUARD, useClass: JwtAuthGuard }
     ]
 })
 export class AuthModule { }
--- src/infra/http/user/controller/login.controller.ts
+++ src/infra/http/user/controller/login.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, HttpCode, Post, Res } from "@nestjs/common";
 import { Response } from "express";
 import { AuthDto } from "src/domain/user/dto/auth.dto";
 import { AuthUserUseCase } from "src/domain/user/use-case/auth.user";
+import { Public } from "src/infra/auth/public";
 import { UserPresenter } from "src/infra/presenters/user.presenter";
 
 @Controller('login')
@@ -9,6 +10,7 @@ export class LoginController {
 
     constructor(private readonly authService: AuthUserUseCase) { }
 
+    @Public()
     @Post()
     @HttpCode(200)
     async handler(@Body() loginBody: AuthDto, @Res() response: Response) {
